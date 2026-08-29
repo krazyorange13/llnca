d76ca1e81a7bf302add682c1aa6e60b796662142
krazyorange13 <77130315+krazyorange13@users.noreply.github.com>
2026-08-29 15:15:55 -0500

    update notebook dataset

--- prebuild.py
+++ prebuild.py
@@ -1,3 +1,3 @@
-!mkdir -p data/ezpz/
-!wget https://files.krazyorange.dev/data/ezpz/ezpz.corptok.txt -O data/ezpz/ezpz.corptok.txt
-!wget https://files.krazyorange.dev/data/ezpz/ezpz.vocab.json -O data/ezpz/ezpz.vocab.json
+!mkdir -p data/ezpz2/
+!wget https://files.krazyorange.dev/data/ezpz2/ezpz2.corptok.txt -O data/ezpz2/ezpz2.corptok.txt
+!wget https://files.krazyorange.dev/data/ezpz2/ezpz2.vocab.json -O data/ezpz2/ezpz2.vocab.json
