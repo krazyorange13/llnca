c7a4b03c602969f4b7bb05a32be897a1e7b0a46a
krazyorange13 <77130315+krazyorange13@users.noreply.github.com>
2026-08-31 18:05:34 -0500

    add jupyter visdom dependency

--- prebuild.py
+++ prebuild.py
@@ -2,3 +2,4 @@
 !mkdir -p data/ezpz2/
 !wget https://files.krazyorange.dev/data/ezpz2/ezpz2.corptok.txt -O data/ezpz2/ezpz2.corptok.txt
 !wget https://files.krazyorange.dev/data/ezpz2/ezpz2.vocab.json -O data/ezpz2/ezpz2.vocab.json
+!pip install visdom
